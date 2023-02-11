82087ea19798dd1df48df0c33680df446dde12ec
Shayan1319 <98160751+Shayan1319@users.noreply.github.com>
2023-02-11 11:53:22 +0500

    first commit2/11/2023

--- hrdash/employeePersonalInfo.php
+++ hrdash/employeePersonalInfo.php
@@ -19,7 +19,7 @@
               </div>
               <br>
           <!-- /.card-header -->
-              <div class="card-body ">
+              <div class="card-body"  id="section1">
                 <div class="row mt-5">
                     <div class="col-md-4">
                       <div class="form-group">
@@ -42,7 +42,7 @@
                     <div class="col-md-4">
                       <div class="form-group">
                         <label>Middle Name</label>
-                        <input id="mName" type="text" name="" placeholder="Middle Name" class="form-control" autocomplete="off" required="">
+                        <input id="mName" type="text" name="" placeholder="Middle Name" class="form-control" autocomplete="off">
                       </div>
                     </div>
                       <div class="col-md-4">
@@ -54,7 +54,7 @@
                     <div class="col-md-4">
                       <div class="form-group">
                         <label>Father Name</label>
-                        <input id="lName" type="text" name="" placeholder="Last Name" class="form-control" autocomplete="off" required="">
+                        <input id="FaName" type="text" name="" placeholder="Father Name" class="form-control" autocomplete="off" required="">
                       </div>
                     </div> 
                     <div class="col-md-4">
@@ -72,63 +72,63 @@
                     <div class="col-md-4">
                       <div class="form-group">
                         <label>Permenent Address</label>
-                        <input type="text" name="" placeholder="Permenent Address" class="form-control" autocomplete="off" required="">
+                        <input id="PAddress" type="text" name="" placeholder="Permenent Address" class="form-control" autocomplete="off" required="">
                       </div>
                     </div>
                     <div class="col-md-4">
                       <div class="form-group">
                         <label>Current Address</label>
-                        <input type="text" name="" placeholder="Current Address" class="form-control" autocomplete="off" required="">
+                        <input id="CAddress" type="text" name="" placeholder="Current Address" class="form-control" autocomplete="off" required="">
                       </div>
                     </div>
                     <div class="col-md-4">
                       <div class="form-group">
                         <label>City</label>
-                        <input type="text" name="" placeholder="City" class="form-control" autocomplete="off" required="">
+                        <input id="city" type="text" name="" placeholder="City" class="form-control" autocomplete="off" required="">
                       </div>
                     </div>
                     <div class="col-md-4">
                       <div class="form-group">
                         <label>Postal Address</label>
-                        <input type="text" name="" placeholder="Postal Address" class="form-control" autocomplete="off" required="">
+                        <input id="PAddress" type="text" name="" placeholder="Postal Address" class="form-control" autocomplete="off" required="">
                       </div>
                     </div>
                   
                     <div class="col-md-4">
                       <div class="form-group">
                         <label>Mobile Number</label>
-                        <input type="text" name="" placeholder="Mobile Number" class="form-control" autocomplete="off" required="">
+                        <input id="moNum" type="text" name="" placeholder="Mobile Number" class="form-control" autocomplete="off" required="">
                       </div>
                     </div>
                       <div class="col-md-4">
                       <div class="form-group">
                         <label>Office Phone Number</label>
-                        <input type="text" name="" placeholder="Office Number" class="form-control" autocomplete="off" required="">
+                        <input id="OfPNum" type="text" name="" placeholder="Office Number" class="form-control" autocomplete="off" required="">
                       </div>
                     </div>
                       <div class="col-md-4">
                       <div class="form-group">
                         <label>Alternate Number</label>
-                        <input type="text" name="" placeholder="Alternate Number" class="form-control" autocomplete="off" required="">
+                        <input id="ANum" type="text" name="" placeholder="Alternate Number" class="form-control" autocomplete="off" required="">
                       </div>
                     </div>
                     <div class="col-md-4">
                       <div class="form-group">
                         <label>Date of Birth</label>
-                        <input type="Date" name="" class="form-control" autocomplete="off" required="">
+                        <input id="DofB" type="Date" name="" class="form-control" autocomplete="off" required="">
                       </div>
                     </div>
                     <div class="col-md-4">
                       <div class="form-group">
                         <label>Religion</label>
-                        <input type="text" name="" placeholder="Religion" class="form-control" autocomplete="off" required="">
+                        <input id="Religion" type="text" name="" placeholder="Religion" class="form-control" autocomplete="off" required="">
                       </div>
                     </div>
                   
                         <div class="col-md-4">
                       <div class="form-group">
                         <label>Gender</label>
-                        <select name="" id="" class="form-control select2">
+                        <select id="Gender" name="" id="" class="form-control select2">
                             <option value="">Choose</option>
                             <option value="">Male</option>
                             <option value="">Female</option>
@@ -139,20 +139,20 @@
                     <div class="col-md-4">
                       <div class="form-group">
                         <label>Blood Group</label>
-                        <input type="text" name="" placeholder="Blood Group" class="form-control" autocomplete="off" required="">
+                        <input id="BlGroup" type="text" name="" placeholder="Blood Group" class="form-control" autocomplete="off" required="">
                       </div>
                     </div>
                       <div class="col-md-4">
                       <div class="form-group">
                         <label>Domicile</label>
-                        <input type="text" name="" placeholder="Domicile" class="form-control" autocomplete="off" required="">
+                        <input id="Domicile" type="text" name="" placeholder="Domicile" class="form-control" autocomplete="off" required="">
                       </div>
                     </div>
                     
                     <div class="col-md-4">
                       <div class="form-group">
                         <label>Marital Status </label>
-                        <select name="" id="" class="form-control select2">
+                        <select name="" id="MaritalStatus" class="form-control select2">
                             <option value="">Choose</option>
                             <option value=""> Married</option>
                             <option value=""> Unmarried</option>
@@ -164,28 +164,29 @@
                       <div class="col-md-4">
                       <div class="form-group">
                         <label>Next of Kin</label>
-                        <input type="text" name="" placeholder="Next of Kin" class="form-control" autocomplete="off" required="">
+                        <input id="NextofKin" type="text" name="" placeholder="Next of Kin" class="form-control" autocomplete="off" required="">
                       </div>
                     </div>
                       <div class="col-md-4">
                       <div class="form-group">
                         <label>Next of Kin Cell Number</label>
-                        <input type="text" name="" placeholder="Next of Kin Cell Number " class="form-control" autocomplete="off" required="">
+                        <input id="NextofKinCellNumber" type="text" name="" placeholder="Next of Kin Cell Number " class="form-control" autocomplete="off" required="">
                       </div>
                     </div>                    <div class="col-md-4">
                       <div class="form-group">
                         <label>Contact Person</label>
-                        <input type="text" name="" placeholder="Contact Person " class="form-control" autocomplete="off" required="">
+                        <input id="ContactPerson" type="text" name="" placeholder="Contact Person " class="form-control" autocomplete="off" required="">
                       </div>
                     </div>                    <div class="col-md-4">
                       <div class="form-group">
                         <label>Contact Person Cell Number</label>
-                        <input type="text" name="" placeholder="Contact Person Cell Number " class="form-control" autocomplete="off" required="">
+                        <input id="CPCN" type="text" name="" placeholder="Contact Person Cell Number " class="form-control" autocomplete="off" required="">
                       </div>
                     </div>
                     
                     </div>                      <div class=" text-end">
-                      <input style="background-color: darkblue;" type="submit" class="btn text-white shadow float-right" value="Submit" name="saveUser1">
+                      
+                      <button type="Next" style="background-color: darkblue;" class="btn text-white shadow float-right" value="Submit" name="saveUser1">Next</button>
                     </div>                    </div>                
             
             </div>
--- hrdash/link/desigene/script.php
+++ hrdash/link/desigene/script.php
@@ -1,3 +1,58 @@
+<script>
+        function validateSection1() {
+            var log1 = document.getElementById("file1").value;
+            var fName = document.getElementById("fName").value;
+            var mName = document.getElementById("mName").value;
+            var lName = document.getElementById("lName").value;
+            var faName = document.getElementById("faName").value;
+            var cNo = document.getElementById("cNo").value;
+            var email = document.getElementById("email").value;
+            var CAddress = document.getElementById("CAddress").value;
+            var city = document.getElementById("city").value;
+            var PAddress = document.getElementById("PAddress").value;
+            var moNum = document.getElementById("moNum").value;
+            var OfPNum = document.getElementById("OfPNum").value;
+            var ANum = document.getElementById("ANum").value;
+            var DofB = document.getElementById("DofB").value;
+            var Religion = document.getElementById("Religion").value;
+            var Gender = document.getElementById("Gender").value;
+            var BlGroup = document.getElementById("BlGroup").value;
+            var Domicile = document.getElementById("Domicile").value;
+            var MaritalStatus = document.getElementById("MaritalStatus").value;
+            var NextofKin = document.getElementById("NextofKin").value;
+            var NextofKinCellNumber = document.getElementById("NextofKinCellNumber").value;
+            var ContactPerson = document.getElementById("ContactPerson").value;
+            var cPCN = document.getElementById("CPCN").value;
+            var vareable1 ="log1 && fName && mName && lName && faName && cNo && email && CAddress && city && PAddress && moNum && OfPNum && ANum && DofB && Religion && Gender && BlGroup && Domicile && MaritalStatus && NextofKin && NextofKinCellNumber && ContactPerson && cPCN";
+            if (vareable1) {
+                document.getElementById("section1").style.display = "none";
+                document.getElementById("section2").style.display = "block";
+            } else {
+                alert("Please fill in the required fields");
+            }
+        }
+
+        function validateSection2() {
+            var age = document.getElementById("age").value;
+            var gender = document.getElementById("gender").value;
+            if (age && gender) {
+                document.getElementById("section2").style.display = "none";
+                document.getElementById("section3").style.display = "block";
+            } else {
+                alert("Please fill in the required fields");
+            }
+        }
+
+        function backToSection1() {
+            document.getElementById("section2").style.display = "none";
+            document.getElementById("section1").style.display = "block";
+        }
+
+        function backToSection2() {
+            document.getElementById("section3").style.display = "none";
+            document.getElementById("section2").style.display = "block";
+        }
+</script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
